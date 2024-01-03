989e7d79fb52c784eb3534cfa74661b38265c698
Prabin120 <87239578+Prabin120@users.noreply.github.com>
2024-01-04 00:58:41 +0530

    Add files via upload

--- home/models.py
+++ home/models.py
@@ -1,23 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.postgres.search import SearchVectorField,SearchVector
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
 # Create your models here.
 
 class Note(models.Model):
-    user = models.ForeignKey(User,on_delete = models.CASCADE)
-    title = models.CharField(max_length = 200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
     content = models.TextField()
-    shared_with = models.ManyToManyField(User,related_name="shared_notes",blank=True,null=True)
-
-    # Added a SearchVectorField for text indexing
+    shared_with = models.ManyToManyField(User, related_name="shared_notes", blank=True)
+    
+    # Add a SearchVectorField for text indexing
     search_vector = SearchVectorField(null=True, blank=True)
-    def save(self, *args, **kwargs):
-        # Update the SearchVectorField whenever the model is saved
-        self.search_vector = SearchVector('content')
-        super().save(*args, **kwargs)
-
 
     def __str__(self):
         return f"{self.title}"
+
+@receiver(post_save, sender=Note)
+def update_search_vector(sender, instance, **kwargs):
+    # Update the SearchVectorField after the instance has been saved
+    Note.objects.filter(pk=instance.pk).update(search_vector=SearchVector('title', 'content'))
+
+
 
     
