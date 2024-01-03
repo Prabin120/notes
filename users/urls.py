bd71119a08946e3a69799cfef64aafaae00351af
Prabin120 <87239578+Prabin120@users.noreply.github.com>
2024-01-04 00:45:16 +0530

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from rest_framework_simplejwt import views as jwt_views
 from .views import SignUpView
 
 urlpatterns = [
-    path('/signup', SignUpView.as_view(), name='auth-signup'),
-    path('/login', jwt_views.TokenObtainPairView.as_view(), name='login'),
-    path('/login/refresh', jwt_views.TokenRefreshView.as_view(), name='login_refresh'),
+    path('signup', SignUpView.as_view(), name='auth-signup'),
+    path('login', jwt_views.TokenObtainPairView.as_view(), name='login'),
+    path('login/refresh', jwt_views.TokenRefreshView.as_view(), name='login_refresh'),
 ]   
